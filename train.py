ad1e3c44d80a6d44abdc41d7438f81bd0693a2a2
jrchang4 <jrchang@stanford.edu>
2021-10-29 11:33:55 -0700

    self

--- train.py
+++ train.py
@@ -20,21 +20,21 @@ class Classifier():
               loss = 'binary_crossentropy',
               metrics=['AUC', 'accuracy', self.recall_m, self.precision_m, self.f1_m])
     
-  def recall_m(y_true, y_pred):
+  def recall_m(self, y_true, y_pred):
         true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
         possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
         recall = true_positives / (possible_positives + K.epsilon())
         return recall
 
-  def precision_m(y_true, y_pred):
+  def precision_m(self, y_true, y_pred):
       true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
       predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
       precision = true_positives / (predicted_positives + K.epsilon())
       return precision
 
-  def f1_m(y_true, y_pred):
-      precision = precision_m(y_true, y_pred)
-      recall = recall_m(y_true, y_pred)
+  def f1_m(self, y_true, y_pred):
+      precision = self.precision_m(y_true, y_pred)
+      recall = self.recall_m(y_true, y_pred)
       return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
   def binary_get_fp_and_fn_filenames(self, val_data):
